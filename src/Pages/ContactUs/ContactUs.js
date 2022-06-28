b0789ceaf63239d53758a7a9c98799d2a25f8ead
rashidul191 <rashidul.191cse.gub@gmail.com>
2022-06-28 22:58:28 +0600

    edit contact us pages

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -13,13 +13,13 @@ const ContactUs = () => {
         }
     }
   return (
-    <div className="container my-5">
-      <h2 className="text-center text-info">Contact Us</h2>
+    <div style={{backgroundColor:" #f4f5f7"}} className="container my-5 p-5">
+      <h2 className="text-center">Contact Us</h2>
       <Form onSubmit={handleMassageSubmit} className="w-50 mx-auto">
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address: <span className="text-danger">*</span> </Form.Label>
           <Form.Control type="email" name="email" placeholder="enter email" required/>
-        </Form.Group>
+        </Form.Group >
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Message: <span className="text-danger">*</span></Form.Label>
           <Form.Control as="textarea" name="massage" rows={3}  required/>
